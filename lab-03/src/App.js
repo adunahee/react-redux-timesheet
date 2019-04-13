dca08ee9238b24b333ff0c7e6fd361f89583e879
Anthony D <dunahee@gmail.com>
2019-04-13 14:31:54 -0500

    added counter to eomployees page to pracice better use of setState function

--- lab-03/src/App.js
+++ lab-03/src/App.js
@@ -7,21 +7,38 @@ import Timesheets from './timesheets/Timesheets';
 import Navigation from './nav/Navigation';
 
 class App extends React.Component {
+  state = {
+    counter: 0,
+  }
+  crementor = (val, e) => {
+    // this.setState({counter: this.state.counter + val});
+    // the better way to do it to ensure events dont get skipped and that the 
+    // prev state is always referenced when running the next set state
+    this.setState((state) => {
+      return { counter: state.counter + val }
+    })
+  }
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <div className="container">
-          <Navigation />
-            <Switch>
-              <Route exact path='/projects' component={Projects} />
-              <Route exact path='/employees' component={Employees} />
-              <Route exact path='/timesheets' component={Timesheets} />
-              <Redirect to='/employees' />
-            </Switch>
+      <div>
+        <BrowserRouter>
+          <div className="App">
+            <div className="container">
+              <Navigation />
+              <Switch>
+                <Route exact path='/projects' component={Projects} />
+                <Route exact path='/employees' component={Employees} />
+                <Route exact path='/timesheets' component={Timesheets} />
+                <Redirect to='/employees' />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+        <h2>{this.state.counter}</h2>
+        <button onClick={this.crementor.bind(this, -1)}>-</button>
+        <button onClick={this.crementor.bind(this, 1)}>+</button>
+      </div>
+
     );
   }
 }
